251ac0fdc4f18bc406570ae0ca6a92d42c5b1ef3
Yuriy Vinogradov <yuriy.vinogradov@mcbservices.ru>
2014-04-24 17:49:30 +0400

    calendar in progress

--- CODAView/src/main/java/net/yvin/codaview/app/activity/CalendarActivity.java
+++ CODAView/src/main/java/net/yvin/codaview/app/activity/CalendarActivity.java
@@ -1,9 +1,12 @@
 package net.yvin.codaview.app.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 import net.yvin.codaview.app.R;
 import net.yvin.codaview.app.activity.base.MenuAbstractActivity;
 
@@ -16,32 +19,33 @@ public class CalendarActivity extends MenuAbstractActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         setMonthTitleColor();
+        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            public void onSelectedDayChange(CalendarView view,
+                                            int year, int month, int dayOfMonth) {
+                Intent intent = new Intent(getApplicationContext(), DailyActivity.class);
+                Toast.makeText(getApplicationContext(),
+                        dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
-    protected void setMonthTitleColor() {
-        try
-        {
-            CalendarView cv = (CalendarView) this.findViewById(R.id.calendarView1);
+    private void setMonthTitleColor() {
+        try {
+            CalendarView cv = (CalendarView) this.findViewById(R.id.calendarView);
             Class<?> cvClass = cv.getClass();
             Field field = cvClass.getDeclaredField("mMonthName");
             field.setAccessible(true);
 
-            try
-            {
+            try {
                 TextView tv = (TextView) field.get(cv);
                 tv.setTextColor(Color.RED);
-            }
-            catch (IllegalArgumentException e)
-            {
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
-            catch (IllegalAccessException e)
-            {
-                e.printStackTrace();
-            }
-        }
-        catch (NoSuchFieldException e)
-        {
+        } catch (NoSuchFieldException e) {
             e.printStackTrace();
         }
     }
---
+++ CODAView/src/main/java/net/yvin/codaview/app/utils/DateUtils.java
@@ -0,0 +1,7 @@
+package net.yvin.codaview.app.utils;
+
+/**
+ * Created by Yuriy on 24.04.2014.
+ */
+public class DateUtils {
+}
