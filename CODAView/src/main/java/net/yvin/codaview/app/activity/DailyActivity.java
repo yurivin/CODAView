acab30701abc5b6b968493b0316e5612ce46c2d0
Iury V. Vinogradov <discours@list.ru>
2014-05-17 15:05:50 +0400

    refactoring of constants and activity names

--- CODAView/src/main/java/net/yvin/codaview/app/activity/DailyActivity.java
+++ CODAView/src/main/java/net/yvin/codaview/app/activity/DailyActivity.java
@@ -10,6 +10,7 @@ import net.yvin.codaview.app.R;
 import net.yvin.codaview.app.repository.DailyContentRepository;
 import net.yvin.codaview.app.repository.DiaryTitlesRepository;
 import net.yvin.codaview.app.utils.AssetsTxtReader;
+import net.yvin.codaview.app.utils.Constants;
 import net.yvin.codaview.app.utils.DateUtils;
 import net.yvin.codaview.app.utils.PropertiesFileReader;
 
@@ -31,9 +32,9 @@ public class DailyActivity extends MenuAbstractActivity {
         if (getIntent() != null) {
             Intent intent = getIntent();
             if (intent.getExtras() != null) {
-                if (intent.getExtras().containsKey("dailyId")) {
-                    dailyId = intent.getStringExtra("dailyId");
-                    Log.d("dailyId", dailyId);
+                if (intent.getExtras().containsKey(Constants.DAILYID)) {
+                    dailyId = intent.getStringExtra(Constants.DAILYID);
+                    Log.d(Constants.DAILYID, dailyId);
                 }
             }
         }
@@ -46,10 +47,10 @@ public class DailyActivity extends MenuAbstractActivity {
         dateTv =(TextView) findViewById(R.id.dateTv);
         dateTv.setText(now.monthDay + "-" + String.valueOf(now.month + 1) + "-" + now.year);
         quoteTv = (TextView) findViewById(R.id.quoteTv);
-        quoteTv.setText(dailyContentRepo.find(dailyId + "-" + "quote", this));
+        quoteTv.setText(dailyContentRepo.find(dailyId + "-" + Constants.QUOTE, this));
         contentTv = (TextView) findViewById(R.id.contentTv);
-        contentTv.setText(dailyContentRepo.find(dailyId + "-" + "main", this));
+        contentTv.setText(dailyContentRepo.find(dailyId + "-" + Constants.MAIN, this));
         sumTv = (TextView) findViewById(R.id.sumTv);
-        sumTv.setText(dailyContentRepo.find(dailyId + "-" + "sum", this));
+        sumTv.setText(dailyContentRepo.find(dailyId + "-" + Constants.SUM, this));
     }
 }
--- CODAView/src/main/java/net/yvin/codaview/app/utils/Constants.java
+++ CODAView/src/main/java/net/yvin/codaview/app/utils/Constants.java
@@ -9,4 +9,9 @@ public class Constants {
     public static final String TRADITIONS = "traditions";
     public static final String PROMISES = "Promises";
 
+    public static final String DAILYID = "dailyId";
+
+    public static final String QUOTE = "quote";
+    public static final String MAIN = "main";
+    public static final String SUM = "sum";
 }
