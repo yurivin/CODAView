ae00039091e8683f7fc77124c1bc39d65b73c586
Iury V. Vinogradov <discours@list.ru>
2014-05-21 14:17:19 +0400

    UI changes

--- CODAView/src/main/java/net/yvin/codaview/app/activity/TwelveActivity.java
+++ CODAView/src/main/java/net/yvin/codaview/app/activity/TwelveActivity.java
@@ -16,6 +16,7 @@ import static net.yvin.codaview.app.utils.Constants.*;
  * Created by Юрий on 12.05.2014.
  */
 public class TwelveActivity extends MenuListAbstractActivity {
+    ArrayAdapter<String> adapter;
 
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -23,24 +24,28 @@ public class TwelveActivity extends MenuListAbstractActivity {
         String[] values = null;
         values = getTwelveArray(values);
 
-        for(int i = 1 ; i <= values.length; i++) {
-            values[i-1] = String.valueOf(i) + ". " + values[i-1];
+        for (int i = 1; i <= values.length; i++) {
+            values[i - 1] = String.valueOf(i) + ". " + values[i - 1];
         }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                R.layout.text_list_item, values);
         setListAdapter(adapter);
     }
 
     private String[] getTwelveArray(String[] values) {
-        switch(getIntent().getStringExtra(Constants.TWELVE)) {
-            case STEPS :
+        switch (getIntent().getStringExtra(Constants.TWELVE)) {
+            case STEPS:
                 values = getResources().getStringArray(R.array.steps);
+                adapter = new ArrayAdapter<String>(this,
+                        R.layout.clickable_text_list_item, values);
                 break;
-            case TRADITIONS :
+            case TRADITIONS:
                 values = getResources().getStringArray(R.array.traditions);
+                adapter = new ArrayAdapter<String>(this,
+                        R.layout.text_list_item, values);
                 break;
-            case PROMISES :
+            case PROMISES:
                 values = getResources().getStringArray(R.array.promises);
+                adapter = new ArrayAdapter<String>(this,
+                        R.layout.text_list_item, values);
                 break;
         }
         return values;
@@ -48,11 +53,14 @@ public class TwelveActivity extends MenuListAbstractActivity {
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        String item = (String) getListAdapter().getItem(position);
-        Toast.makeText(this, item + " выбран", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, StepActivity.class);
-        intent.putExtra(Constants.TWELVE, getIntent().getStringExtra(Constants.TWELVE));
-        intent.putExtra(Constants.NUMBER, position + 1);
-        startActivity(intent);
+        switch (getIntent().getStringExtra(Constants.TWELVE)) {
+            case Constants.STEPS:
+                Intent intent = new Intent(this, StepActivity.class);
+                intent.putExtra(Constants.TWELVE, getIntent().getStringExtra(Constants.TWELVE));
+                intent.putExtra(Constants.NUMBER, position + 1);
+                startActivity(intent);
+                finish();
+                break;
+        }
     }
 }
