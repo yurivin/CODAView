2ef866f8674cba6a89441bb54dc52a219bbc11dc
Yuriy.Vinogradov <Yuriy.Vinogradov@billing.ru>
2014-07-17 17:52:03 +0400

    development of Emotions diary - emotionsRose activity

---
+++ CODAView/src/main/java/net/yvin/codaview/app/activity/EmotionFlowerActivity.java
@@ -0,0 +1,22 @@
+package net.yvin.codaview.app.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import net.yvin.codaview.app.R;
+import net.yvin.codaview.app.activity.base.MenuAbstractActivity;
+import net.yvin.codaview.app.utils.Constants;
+import net.yvin.codaview.app.utils.DateUtils;
+
+/**
+ * Created by Yuriy.Vinogradov on 17.07.2014.
+ */
+public class EmotionFlowerActivity extends MenuAbstractActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emotionflower);
+
+        }
+}
