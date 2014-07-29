79b4e0e06165a4b43416e836e2fc0d23150c6cbc
Yuriy.Vinogradov <Yuriy.Vinogradov@billing.ru>
2014-07-29 15:17:21 +0400

    Date and time setting for feelings diary entry in progress

--- CODAView/src/main/java/net/yvin/codaview/app/activity/NewFeelingsDiaryEntryActivity.java
+++ CODAView/src/main/java/net/yvin/codaview/app/activity/NewFeelingsDiaryEntryActivity.java
@@ -1,9 +1,16 @@
 package net.yvin.codaview.app.activity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import net.yvin.codaview.app.R;
 import net.yvin.codaview.app.activity.base.MenuAbstractActivity;
+
+import java.util.Calendar;
 
 /**
  * Created by Юрий on 27.07.2014.
@@ -17,7 +24,36 @@ public class NewFeelingsDiaryEntryActivity extends MenuAbstractActivity {
     }
 
     public void clickBtnTimeFrom(View v) {
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+        int mHour = c.get(Calendar.HOUR_OF_DAY);
+        int mMinute = c.get(Calendar.MINUTE);
 
+        TimePickerDialog tpd = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+                        Log.d("Time from dialog",hourOfDay + ":" + minute);
+                    }
+                }, mHour, mMinute, true);
+        tpd.show();
+        
+        DatePickerDialog dpd = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        Log.d("Date from dialog",dayOfMonth + "-"
+                                + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        dpd.show();
     }
 
     public void clickBtnTimeTo(View v) {
