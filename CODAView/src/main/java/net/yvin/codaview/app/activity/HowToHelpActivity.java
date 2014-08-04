e9db6431bc3d1589c573885f7fd6aa04092d81d5
Yuriy.Vinogradov <Yuriy.Vinogradov@billing.ru>
2014-08-04 14:24:01 +0400

    How to help by translation

--- CODAView/src/main/java/net/yvin/codaview/app/activity/HowToHelpActivity.java
+++ CODAView/src/main/java/net/yvin/codaview/app/activity/HowToHelpActivity.java
@@ -1,6 +1,9 @@
 package net.yvin.codaview.app.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import net.yvin.codaview.app.R;
 import net.yvin.codaview.app.activity.base.MenuAbstractActivity;
 
@@ -13,5 +16,12 @@ public class HowToHelpActivity extends MenuAbstractActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_howtohelp);
+    }
+
+    public void clickBtnHelpByTranslation(View v) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", "discours@list.ru", null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+        startActivity(Intent.createChooser(emailIntent, "Send email..."));
     }
 }
