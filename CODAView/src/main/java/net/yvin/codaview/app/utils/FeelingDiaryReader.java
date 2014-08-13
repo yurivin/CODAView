4e7acae9bde32c9c6b1ccd536d3e82bf75bbdca0
Yuriy.Vinogradov <Yuriy.Vinogradov@billing.ru>
2014-08-13 17:27:17 +0400

    Feelings diary entry in progress

--- CODAView/src/main/java/net/yvin/codaview/app/utils/FeelingDiaryReader.java
+++ CODAView/src/main/java/net/yvin/codaview/app/utils/FeelingDiaryReader.java
@@ -32,6 +32,8 @@ public class FeelingDiaryReader {
                     builder.append(part);
                     builder.append(",");
                 }
+                FeelingsDiaryEntry feelingsDiaryEntryBean = new FeelingsDiaryEntry(entry[0], entry[1], entry[2], entry[3],
+                        entry[4], entry[5], entry[6], entry[7], entry[8], entry[9], entry[10], entry[11], entry[12]);
                 Log.d("CSV entry: ", builder.toString());
             }
         } catch (FileNotFoundException e) {
