dbc13c2dba1c201151b6bf3a8a09d815486a689c
Yuriy.Vinogradov <Yuriy.Vinogradov@billing.ru>
2014-08-14 13:54:33 +0400

    Feelings diary entry in progress

--- CODAView/src/main/java/net/yvin/codaview/app/activity/FeelingsDiaryActivity.java
+++ CODAView/src/main/java/net/yvin/codaview/app/activity/FeelingsDiaryActivity.java
@@ -1,20 +1,77 @@
 package net.yvin.codaview.app.activity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
 import net.yvin.codaview.app.R;
 import net.yvin.codaview.app.activity.base.MenuAbstractActivity;
+import net.yvin.codaview.app.activity.model.FeelingsDiaryEntry;
 import net.yvin.codaview.app.activity.utils.ActivityLuncher;
+import net.yvin.codaview.app.service.DiaryService;
+import net.yvin.codaview.app.utils.FeelingDiaryReader;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Юрий on 27.07.2014.
  */
 public class FeelingsDiaryActivity extends MenuAbstractActivity {
 
+    ExpandableListView expListView;
+    ArrayList<Map<String, String>> groupData;
+    ArrayList<Map<String, String>> childDataItem;
+    ArrayList<ArrayList<Map<String, String>>> childData;
+    Map<String, String> m;
+    String[] feelingsTitles;
+    String[] feelingsContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feelings);
+        expListView = (ExpandableListView) findViewById(R.id.entries);
+        Resources res = getResources();
+        getData();
+        String[] groupFrom = new String[]{"title"};
+        int[] groupTo = new int[]{android.R.id.text1};
+        String[] childFrom = new String[]{"content"};
+        int[] childTo = new int[]{android.R.id.text1};
+        groupData = new ArrayList<>();
+        for (String group : feelingsTitles) {
+            m = new HashMap<>();
+            m.put("title", group);
+            groupData.add(m);
+        }
+        childData = new ArrayList<>();
+        for (String content : feelingsContent) {
+            childDataItem = new ArrayList<>();
+            m = new HashMap<>();
+            m.put("content", content);
+            childDataItem.add(m);
+            childData.add(childDataItem);
+        }
+        SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+                this,
+                groupData,
+                android.R.layout.simple_expandable_list_item_1,
+                groupFrom,
+                groupTo,
+                childData,
+                android.R.layout.simple_list_item_1,
+                childFrom,
+                childTo);
+        expListView.setAdapter(adapter);
+    }
+
+    private void getData() {
+        Set<FeelingsDiaryEntry> diaryEntries = FeelingDiaryReader.readAll();
+        feelingsTitles = DiaryService.getTitles(diaryEntries);
+        feelingsContent = DiaryService.getContent(diaryEntries);
     }
 }
---
+++ CODAView/src/main/java/net/yvin/codaview/app/service/DiaryService.java
@@ -0,0 +1,19 @@
+package net.yvin.codaview.app.service;
+
+import net.yvin.codaview.app.activity.model.FeelingsDiaryEntry;
+
+import java.util.Set;
+
+/**
+ * Created by Yuriy.Vinogradov on 14.08.2014.
+ */
+public class DiaryService {
+
+    public static String[] getTitles(Set<FeelingsDiaryEntry> diaryEntries) {
+
+    }
+
+    public static String[] getContent(Set<FeelingsDiaryEntry> diaryEntries) {
+
+    }
+}
