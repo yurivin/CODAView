7d6036861a2c88bcad4bd9aa5e46e6ff06341b31
Yuriy.Vinogradov <Yuriy.Vinogradov@billing.ru>
2014-08-29 15:56:50 +0400

    - filter by feelings rating ready

--- CODAView/src/main/java/net/yvin/codaview/app/activity/feelingsDiary/feelingsDiaryFilterService.java
+++ CODAView/src/main/java/net/yvin/codaview/app/activity/feelingsDiary/feelingsDiaryFilterService.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import net.yvin.codaview.app.R;
 import net.yvin.codaview.app.activity.model.FeelingsDiaryEntry;
+import net.yvin.codaview.app.filters.FeelingsDiaryFilter;
 import net.yvin.codaview.app.filters.FilterByIntensity;
+import net.yvin.codaview.app.filters.FilterByRate;
 import net.yvin.codaview.app.utils.ActivityUtils;
 
 import java.util.ArrayList;
@@ -20,26 +22,32 @@ public class FeelingsDiaryFilterService {
 
     List<FeelingsDiaryEntry> entries;
     Context context;
+    List<Integer> filterNumbers;
+
     FeelingsDiaryFilterService(List<FeelingsDiaryEntry> entries, Context context) {
         this.entries = entries;
         this.context = context;
     }
 
     void understandSelectedFilters(List<Integer> filterNumbers) {
+        this.filterNumbers = filterNumbers;
         for (int integer : filterNumbers) {
             switch (integer) {
                 case 0:
-                    showFilterByIntensityDialog();
+                    showFilterByIntensityDialog(context.getString(R.string.select_intensity), integer);
+                    break;
+                case 1:
+                    showFilterByIntensityDialog(context.getString(R.string.select_rate), integer);
                     break;
             }
         }
     }
 
-    void showFilterByIntensityDialog() {
+    void showFilterByIntensityDialog(String title, final int filterNumber) {
         final boolean[] mCheckedItems = new boolean[6];
         final String[] checkRatingTitles = {"0", "1", "2", "3", "4", "5"};
         final List<Integer> intensityNumbers = new ArrayList<>();
-        new AlertDialog.Builder(context).setTitle(R.string.select_intensity)
+        new AlertDialog.Builder(context).setTitle(title)
                 .setMultiChoiceItems(checkRatingTitles, mCheckedItems,
                         new DialogInterface.OnMultiChoiceClickListener() {
                             @Override
@@ -53,7 +61,7 @@ public class FeelingsDiaryFilterService {
                             @Override
                             public void onClick(DialogInterface dialog,
                                                 int id) {
-                                processFilterByIntensity(intensityNumbers);
+                                processFilterByIntensity(intensityNumbers, filterNumber);
                             }
                         })
                 .setNegativeButton(R.string.cancel,
@@ -68,14 +76,19 @@ public class FeelingsDiaryFilterService {
     }
 
 
-    private void processFilterByIntensity(List<Integer> intensityNumbers) {
+    private void processFilterByIntensity(List<Integer> numbers, int filterNumber) {
         List<FeelingsDiaryEntry> tempAll = new ArrayList<>();
         List<FeelingsDiaryEntry> tempFiltered = new ArrayList<>();
-        for (int intensity : intensityNumbers) {
+        for (int number : numbers) {
             tempAll.addAll(entries);
-            new FilterByIntensity(intensity).filter(tempAll);
+                switch (filterNumber) {
+                    case 0:
+                        new FilterByIntensity(number).filter(tempAll);
+                    case 1:
+                        new FilterByRate(number).filter(tempAll);
+            }
             tempFiltered.addAll(tempAll);
         }
-        ((FeelingsDiaryActivity)context).showData(tempFiltered);
+        ((FeelingsDiaryActivity) context).showData(tempFiltered);
     }
 }
