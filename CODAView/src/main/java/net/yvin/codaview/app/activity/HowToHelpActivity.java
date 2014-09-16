1406e9e0640f16b93b9761cb47bedeb804292d4d
Yuriy.Vinogradov <Yuriy.Vinogradov@billing.ru>
2014-09-16 14:16:30 +0400

    - Rate app on google play functionality

--- CODAView/src/main/java/net/yvin/codaview/app/activity/HowToHelpActivity.java
+++ CODAView/src/main/java/net/yvin/codaview/app/activity/HowToHelpActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import net.yvin.codaview.app.R;
 import net.yvin.codaview.app.activity.base.MenuAbstractActivity;
 
@@ -16,5 +17,23 @@ public class HowToHelpActivity extends MenuAbstractActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_howtohelp);
+    }
+
+    public void rateOnMarket(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("market://details?id=net.yvin.codaview.app"));
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            String appPackageName = "net.yvin.codaview.app";
+            String url = "https://play.google.com/store/apps/details?id=" + appPackageName;
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            try {
+                startActivity(intent);
+            } catch (Exception ee) {
+                Toast.makeText(this, "Some problem happens", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
